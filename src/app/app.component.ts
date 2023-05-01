40c91077760a0c16242aaf69f8a9f7152d7dea5d
ezekielKouassi-dev <ezekielkouassi836@yahoo.com>
2023-05-01 15:22:16 +0000

    code base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-data-matrix',
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'datamatrix';
+export class DataMatrixComponent {
+  imageData!: string;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('http://localhost:8080/api/datamatrix/image', { responseType: 'arraybuffer' })
+      .subscribe(response => {
+        const bytes = new Uint8Array(response);
+        const binary = bytes.reduce((acc, byte) => acc + String.fromCharCode(byte), '');
+        this.imageData = 'data:image/png;base64,' + btoa(binary);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { DataMatrixComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    DataMatrixComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [DataMatrixComponent]
 })
 export class AppModule { }
